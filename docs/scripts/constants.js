6391e302b91540532020261c7cd52afdaa35445e
Mark Wangsgard <mark.wangsgard@students.snow.edu>
2025-11-17 19:30:27 -0700

    BaseURL Update

--- docs/scripts/constants.js
+++ docs/scripts/constants.js
@@ -1,2 +1,2 @@
-export const myApiUrl = "http://127.0.0.1:5001";
-// export const myApiUrl = `https://soutenu-chan-unscheming.ngrok-free.dev/`;
+// export const myApiUrl = "http://127.0.0.1:5001";
+export const myApiUrl = `https://soutenu-chan-unscheming.ngrok-free.dev/`;
