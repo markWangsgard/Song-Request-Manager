ba5cb8d3ae66116570c0e5db7cfc431535ca00f8
Mark Wangsgard <mark.wangsgard@students.snow.edu>
2025-11-21 22:38:48 -0700

    Enhance admin settings: Update element IDs for better accessibility, add event listeners for playlist selection and settings management

--- docs/scripts/admin-settings-ui.js
+++ docs/scripts/admin-settings-ui.js
@@ -1,11 +1,132 @@
+import {
+  allowRepeats,
+  autoAdd,
+  autoAddTime,
+  currentUser,
+  numbOfAllowedRequests,
+  selectedDays,
+  setAllowRepeats,
+  setAutoAdd,
+  setMonday,
+  setTuesday,
+  setWednesday,
+  setThursday,
+  setFriday,
+  setSaturday,
+  setSunday,
+  setNumbOfAllowedRequests,
+  setUser,
+  setAutoAddTime,
+} from "./constants.js";
 import { getMe, login } from "./service.js";
 
 const loginButton = document.getElementById("signIn");
-loginButton.addEventListener("click", async (e) => {
+const selectPlaylistButton = document.getElementById("selectPlaylist");
+const userNameElement = document.getElementById("userName");
+const requestLimitInputElement = document.getElementById("requestLimit");
+const autoAddSwitchElement = document.getElementById("autoAdd");
+const allowRepeatsSwitchElement = document.getElementById("allowRepeats");
+const tuesdayCheckboxElement = document.getElementById("dayTuesday");
+const mondayCheckboxElement = document.getElementById("dayMonday");
+const thursdayCheckboxElement = document.getElementById("dayThursday");
+const wednesdayCheckboxElement = document.getElementById("dayWednesday");
+const saturdayCheckboxElement = document.getElementById("daySaturday");
+const fridayCheckboxElement = document.getElementById("dayFriday");
+const timePickerElement = document.getElementById("timePicker");
+const sundayCheckboxElement = document.getElementById("daySunday");
+
+const addAllEventListeners = async () => {
+  loginButton.addEventListener("click", async (e) => {
     e.preventDefault();
     const user = await login();
-    console.log(user);
-});
+    // setUser(await getMe());
+  });
 
-const me = await getMe();
-console.log(me.profile);
+  selectPlaylistButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    // Add select Playlist function
+  });
+
+  requestLimitInputElement.addEventListener("input", () => {
+    setNumbOfAllowedRequests(requestLimitInputElement.value);
+  });
+
+  allowRepeatsSwitchElement.addEventListener("input", () => {
+    setAllowRepeats(allowRepeatsSwitchElement.checked);
+  });
+
+  autoAddSwitchElement.addEventListener("input", () => {
+    setAutoAdd(autoAddSwitchElement.checked);
+  });
+
+  mondayCheckboxElement.addEventListener("input", () => {
+    setMonday(mondayCheckboxElement.checked);
+  });
+
+  tuesdayCheckboxElement.addEventListener("input", () => {
+    setTuesday(tuesdayCheckboxElement.checked);
+  });
+
+  wednesdayCheckboxElement.addEventListener("input", () => {
+    setWednesday(wednesdayCheckboxElement.checked);
+  });
+
+  thursdayCheckboxElement.addEventListener("input", () => {
+    setThursday(thursdayCheckboxElement.checked);
+  });
+
+  fridayCheckboxElement.addEventListener("input", () => {
+    setFriday(fridayCheckboxElement.checked);
+  });
+
+  saturdayCheckboxElement.addEventListener("input", () => {
+    setSaturday(saturdayCheckboxElement.checked);
+  });
+
+  sundayCheckboxElement.addEventListener("input", () => {
+    setSunday(sundayCheckboxElement.checked);
+  });
+
+  timePickerElement.addEventListener("input", () => {
+    setAutoAddTime(timePickerElement.value);
+  })
+};
+
+const updateUser = async () => {
+  const user = await getMe();
+  if (user) {
+    userNameElement.textContent = `Signed in as ${user.profile.display_name}`;
+    const loginButton = document.getElementById("signIn");
+    loginButton.textContent = "Sign Out";
+    setUser(user);
+  }
+};
+
+const updatePlaylist = () => {
+  // add update playlist function
+};
+
+const loadSettings = () => {
+  updateUser();
+
+  updatePlaylist();
+
+  requestLimitInputElement.value = numbOfAllowedRequests;
+
+  allowRepeatsSwitchElement.checked = allowRepeats;
+
+  autoAddSwitchElement.checked = autoAdd;
+
+  mondayCheckboxElement.checked = selectedDays.monday;
+  tuesdayCheckboxElement.checked = selectedDays.tuesday;
+  wednesdayCheckboxElement.checked = selectedDays.wednesday;
+  thursdayCheckboxElement.checked = selectedDays.thursday;
+  fridayCheckboxElement.checked = selectedDays.friday;
+  saturdayCheckboxElement.checked = selectedDays.saturday;
+  sundayCheckboxElement.checked = selectedDays.sunday;
+
+  timePickerElement.value = autoAddTime;
+};
+
+addAllEventListeners();
+loadSettings();
--- docs/scripts/constants.js
+++ docs/scripts/constants.js
@@ -1,4 +1,59 @@
 export const myApiUrl = "http://127.0.0.1:5001";
 // export const myApiUrl = `https://soutenu-chan-unscheming.ngrok-free.dev`;
-export const numbOfAllowedRequests = 3;
-export const allowRepeats = true;
+
+// settings
+export let currentUser = null;
+export let currentPlaylist = null;
+export let numbOfAllowedRequests = 3;
+export let allowRepeats = true;
+export let autoAdd = false;
+export const selectedDays = {
+    monday: false,
+    tuesday: false,
+    wednesday: true,
+    thursday: false,
+    friday: false,
+    saturday: false,
+    sunday: false
+};
+export let autoAddTime = "22:30"
+
+export const setUser = (user) => {
+    currentUser = user
+};
+export const setCurrentPlaylist = (playlist) => {
+    currentPlaylist = playlist
+}
+export const setNumbOfAllowedRequests = (limit) => {
+    numbOfAllowedRequests = limit;
+}
+export const setAllowRepeats = (allowed) => {
+    allowRepeats = allowed;
+}
+export const setAutoAdd = (auto) => {
+    autoAdd = auto;
+}
+export const setMonday = (monday) => {
+    selectedDays.monday = monday;
+}
+export const setTuesday = (tuesday) => {
+    selectedDays.tuesday = tuesday;
+}
+export const setWednesday = (wednesday) => {
+    selectedDays.wednesday = wednesday;
+}
+export const setThursday = (thursday) => {
+    selectedDays.thursday = thursday;
+}
+export const setFriday = (friday) => {
+    selectedDays.friday = friday;
+}
+export const setSaturday = (saturday) => {
+    selectedDays.saturday = saturday;
+}
+export const setSunday = (sunday) => {
+    selectedDays.sunday = sunday;
+}
+export const setAutoAddTime = (time) => {
+    autoAddTime = time;
+}
